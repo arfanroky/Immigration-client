f07b2b006c0d637901b19b8347057b2dfd2ce0b1
Your Name <you@example.com>
2022-04-18 22:41:26 +0600

    changed folder name

--- src/Pages/HomePage/BannerPage/BannerPage.js
+++ src/Pages/HomePage/BannerPage/BannerPage.js
@@ -8,7 +8,7 @@ const BannerPage = () => {
                 <div className='banner-info'>
                     <h1 style={{fontSize:'4rem', fontWeight:"bold", color:'lightBlue'}}>Immigration Process Starts Here</h1>
                     <p className=''>
-                    Immigration consultants help people immigrate from one country to another for study, work, travel or business purpose. They provide legal services in the area of immigration law as permitted by legislation.
+                    Immigration consultants help people immigrate from one country to another for study, work, travel or business purpose. I provide legal services in the area of immigration law as permitted by legislation.
                     </p>
                     <button className='btn-lg btn-danger px-5'>Discover</button>
                 </div>
--- src/Pages/HomePage/Home/Home.js
+++ src/Pages/HomePage/Home/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Couching from '../../Couching/Couching';
 import Services from '../../Services/Services';
-import Banner from '../Banner/Banner';
 import BannerPage from '../BannerPage/BannerPage';
 
 const Home = () => {
